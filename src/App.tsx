845fa7e3474baca8d912053e8890ea5eba5e78e7
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-08-22 14:25:22 +0200

    Chess board done

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,86 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[tabel , setTabel] = useState([
+    [{figure:"♜", name:"black",canMove:false},
+    {figure:"♞", name:"black",canMove:false},
+    {figure:"♝", name:"black",canMove:false},
+    {figure:"♛", name:"black",canMove:false},
+    {figure:"♚", name:"black",canMove:false},
+    {figure:"♝", name:"black",canMove:false},
+    {figure:"♞", name:"black",canMove:false},
+    {figure:"♜", name:"black",canMove:false}],
+ 
+    [{figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false},
+    {figure:"♟", name:"black-pawn",canMove:false}],
+
+    [{figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false},
+    {figure:"", name:"",canMove:false}],
+ 
+   [{figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false}],
+ 
+   [{figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false}],
+ 
+   [{figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false},
+   {figure:"",name:"",canMove:false}],
+ 
+  
+   [{figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false},
+   {figure:"♙", name:"whitePawn",canMove:false}],
+ 
+   [{figure:"♖", name:"white",canMove:false},
+   {figure:"♘", name:"white",canMove:false},
+   {figure:"♗", name:"white",canMove:false},
+   {figure:"♕", name:"white",canMove:false},
+   {figure:"♔", name:"white",canMove:false},
+   {figure:"♗", name:"white",canMove:false},
+   {figure: "♘", name:"white",canMove:false},
+   {figure:"♖", name:"white",canMove:false}],
+  ])
 
   return (
     <div className="App">
-      <h1>React Project</h1>
+       <div className="chess-tabel" >
+        {tabel.map((row,rowIndex)=>row.map((cel,cellIndex)=>(<div className={`cel ${(rowIndex-cellIndex)%2===0?"white":"black"} ${rowIndex}-${cellIndex} ${cel.canMove}`}>{cel.figure}</div>)))}
+      </div>
     </div>
   )
 }
